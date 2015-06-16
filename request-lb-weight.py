087d612f524464af48871dddc49df79bf4a8305e
Roger Swingle <roger@mark43.com>
2015-06-16 14:34:39 -0400

    cut over request-lb-weight.py to use redis

--- request-lb-weight.py
+++ request-lb-weight.py
@@ -3,7 +3,7 @@
 # Please note the LICENSE file accompanying this script.
 # See https://github.com/reincubate/haproxy-dynamic-weight for more information.
 
-import os, socket, site, sys, memcache
+import os, socket, site, sys, redis
 
 # If the calculation of the load AND the weighting/actioning
 # of the load data is at the same interval (1 minute) then we
@@ -24,8 +24,8 @@ if load > max_load:
 
 weight = int( ( (255 / max_load) * ( ( max_load + 0.001 ) - load ) ) + 1 )
 
-if len(sys.argv) > 2:
+if len(sys.argv) > 3:
     print 'Declaring weight of %s for %s for %ss, given load of %s' % ( weight, hostname, 60 * 2, load )
 
-mc = memcache.Client( [ sys.argv[1] ], )
-mc.set( 'server-weight-%s' % hostname, weight, time=60*2 ) # Set a two minute expiry
+r = redis.StrictRedis(host=sys.argv[1], port=sys.argv[2], db=0)
+r.set( 'server-weight-%s' % hostname, weight)
